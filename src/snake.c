f71aa5aa0831180393950ede7b057f67b0a881c5
yoshiki193 <maromu193@gmail.com>
2024-07-18 06:56:52 +0900

    main

--- src/snake.c
+++ src/snake.c
@@ -21,16 +21,17 @@ struct Feed {
   struct Feed *next;
 };
 
-void run();
+void run(int sy, int sx);
 void makeFeed(struct Feed **f, int sy, int sx);
 int eraseFeed(struct Feed **f, struct Snake *s);
-void showFeed(struct Feed *f);
+void showFeed(struct Feed *f, struct Snake *s);
 void makeSnake(struct Snake **s, int y, int x);
 int moveSnake(struct Snake *s, int ch, int lch, int sy, int sx);
 void addSnake(struct Snake **s, int lch);
 void initSnake(struct Snake **s);
-void printWall(int sy, int sx);
 int collisionDetection(struct Snake *s);
+void printWall(int sy, int sx);
+void showTitle();
 
 int main(int argc, char **argv) {
   initscr();
@@ -38,9 +39,32 @@ int main(int argc, char **argv) {
   cbreak();
   curs_set(0);
   keypad(stdscr,TRUE);
-  run();
+  timeout(50);
+  showTitle();
   endwin();
   return 0;
+}
+
+void showTitle() {
+  int sy;
+  int sx;
+  int ch;
+
+  getmaxyx(stdscr,sy,sx);
+  clear();
+  mvprintw(sy/2-2,(sx-110)/2," oooooooo8  oooo   oooo      o      oooo   oooo ooooooooooo   ooooooo8       o      oooo     oooo ooooooooooo ");
+  mvprintw(sy/2-1,(sx-110)/2,"888          8888o  88      888      888  o88    888    88  o888    88      888      8888o   888   888    88  ");
+  mvprintw(sy/2  ,(sx-110)/2," 888oooooo   88 888o88     8  88     888888      888ooo8    888    oooo    8  88     88 888o8 88   888ooo8    ");
+  mvprintw(sy/2+1,(sx-110)/2,"        888  88   8888    8oooo88    888  88o    888    oo  888o    88    8oooo88    88  888  88   888    oo  ");
+  mvprintw(sy/2+2,(sx-110)/2,"o88oooo888  o88o    88  o88o  o888o o888o o888o o888ooo8888  888ooo888  o88o  o888o o88o  8  o88o o888ooo8888 ");
+
+  while((ch = getch()) != '3') {
+    if(ch == '1') {
+      clear();
+      run(sy,sx);
+      break;
+    }
+  }
 }
 
 void makeFeed(struct Feed **f, int sy, int sx) {
@@ -97,10 +121,21 @@ int eraseFeed(struct Feed **f, struct Snake *s) {
   return flag;
 }
 
-void showFeed(struct Feed *f) {
-  int i = 0;
+void showFeed(struct Feed *f, struct Snake *s) {
+  struct Snake *tmp;
+  int flag = 0;
+
   while(f != NULL) {
-    mvaddch(f->y,f->x,f->component);
+    tmp = s;
+
+    while(tmp != NULL) {
+      if((tmp->y == f->y) && (tmp->x == f->x)) flag = 1;
+      tmp = tmp->next;
+    }
+
+    if(flag == 0) mvaddch(f->y,f->x,f->component);
+
+    flag = 0;
     f = f->next;
   }
 }
@@ -122,7 +157,7 @@ void makeSnake(struct Snake **s, int y, int x) {
 
   new->y = y;
   new->x = x;
-  new->component = '#';
+  new->component = 'o';
   new->next = NULL;
 }
 
@@ -259,32 +294,31 @@ void printWall(int sy, int sx) {
   }
 }
 
-void run() {
+void run(int sy, int sx) {
   int i;
-  int sy;
-  int sx;
   int ch = 0;
   int lch = 0;
   int loop = 0;
+  int t1 = 0;
   int flag = 0;
   int goflag = 0;
   int lv = 1;
   int score = 0;
+  int blink = 0;
+  int feedq = sx/10;
   struct Snake *s = NULL;
   struct Feed *f = NULL;
 
-  getmaxyx(stdscr,sy,sx);
   sx -= 14;
   printWall(sy,sx);
-  timeout(50);
   srand((int)time(NULL));
   initSnake(&s);
-  for(i = 0;i < 5;i++) makeFeed(&f,sy,sx);
+  for(i = 0;i < feedq;i++) makeFeed(&f,sy,sx);
 
   while(((ch = getch()) != 'q')) {
     loop++;
 
-    showFeed(f);
+    showFeed(f, s);
 
     if((ch != KEY_RIGHT) && (ch != KEY_LEFT) && (ch != KEY_UP) && (ch != KEY_DOWN)) {
       lch = moveSnake(s,lch,lch,sy,sx);
@@ -293,6 +327,7 @@ void run() {
     }
 
     goflag = collisionDetection(s);
+
     if(goflag == 1) break;
 
     flag = eraseFeed(&f,s);
@@ -304,11 +339,27 @@ void run() {
 
     if(score >= ((round(pow(lv,2)) * 10))) {
       lv++;
+      t1 = loop;
     }
 
     mvprintw(3,sx + 1,"Lv.%d",lv);
     mvprintw(4,sx + 1,"SCORE.%d",score);
+    if((t1 != 0) && ((loop - t1) != 50)) {
+      if(((loop - t1) % 5) == 0){
+        blink++;
+      }
 
-    if(f == NULL) for(i = 0;i < 5;i++) makeFeed(&f,sy,sx);
+      if(blink%2 == 0) {
+        mvprintw(6,sx + 1,"LEVELUP");
+      } else {
+        mvprintw(6,sx + 1,"       ");
+      }
+    } else {
+      mvprintw(6,sx + 1,"       ");
+        t1 = 0;
+        blink = 0;
+    }
+
+    if(f == NULL) for(i = 0;i < feedq;i++) makeFeed(&f,sy,sx);
   }
 }
